0324d390710301ac300f72ad3fd874ccec739da8
Ashish Mishra <ashishk861@gmail.com>
2020-07-29 23:38:32 -0400

    Create BinaryTreeHeight.java

---
+++ src/RecursionPrograms/BinaryTreeHeight.java
@@ -0,0 +1,11 @@
+package RecursionPrograms;
+
+public class BinaryTreeHeight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//store the height of left in left variable and right in right variable.
+
+	}
+
+}
