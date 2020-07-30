aab5cda9a2a898bd59fab036cb1b2dbf4e2bc904
Ashish Mishra <ashishk861@gmail.com>
2020-07-29 23:38:35 -0400

    Create Number.java

---
+++ src/RecursionPrograms/Number.java
@@ -0,0 +1,23 @@
+package RecursionPrograms;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printNumber(0,8);
+	}
+	
+	public static int printNumber(int i,int n) {
+		int a=0;
+		if(i==n) {
+			System.out.println(i);
+			return n;
+		}else {
+			System.out.println(i);
+			a= printNumber(i+1,n);
+			System.out.println("i"+i);
+		}
+		return a;
+	}
+
+}
